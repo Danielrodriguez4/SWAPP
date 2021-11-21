3cd86164c15219d947a961e43a34190cde1cbd40
Ronaldo Arias Parada <ronaldorafaelap@ufps.edu.co>
2021-11-21 18:58:58 -0500

    Requerimiento de inicio de sesión

---
+++ view/dashboard/dashboard.php
@@ -0,0 +1,23 @@
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+<section class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+          <img src="assets\imagen\ideas.jpg" alt="" class="" style="opacity: .8"  height="200" width="1000">
+          </div>     
+        </div>
+      </div><!-- /.container-fluid -->
+</section>
+<!-- timeline item -->
+              <div>
+                <div class="timeline-item">
+                  <h3 class="timeline-header"><a style="color:black;" href="#">Convocatorias.</a></h3>
+                  <div class="timeline-body">
+                    <div class="embed-responsive embed-responsive-16by9">
+                    <iframe width="460" height="155" src="https://www.youtube.com/embed/GWfgRBwnjhQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    </div>
+                    
+                  </div>
+                </div>
+              </div>
+              <!-- END timeline item -->
